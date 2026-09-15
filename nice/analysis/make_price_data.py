5fc124e99b397e1b555d8080765e4a39c3cd866f
elenya-grant <116225007+elenya-grant@users.noreply.github.com>
2026-09-15 13:47:51 -0600

    other changes to make_price_data

--- nice/analysis/make_price_data.py
+++ nice/analysis/make_price_data.py
@@ -1,12 +1,28 @@
+import warnings
+from pathlib import Path
+
 import numpy as np
 import pandas as pd
 
+from nice import DATA_DIR
+from nice.tools.df_tools import convert_to_type
 from nice.tools.eia_860_file_tools import load_eia_860
 
-net_load_dir = "/projects/surint/campd_spheres/net_load/"
-lmp_dir = ""
+run_local = True
+
+if run_local:
+    price_node_mapper_path = DATA_DIR / "lmp" / "facility_lmp_mapping(in).csv"
+    lmp_dir = DATA_DIR / "lmp"
+    net_load_dir = DATA_DIR / "net_load"
+    net_load_fname = "Net_Load_subset_2025.csv"
+else:
+    net_load_dir = Path("/projects/surint/campd_spheres/net_load")
+    net_load_fname = "Net_Load.csv"
+    lmp_dir = Path("/projects/surint/campd_spheres/lmp_data")
+    price_node_mapper_path = lmp_dir / "facility_lmp_mapping(in).csv"
 
 data_year = 2025
+# load_year = 2025
 
 """
 This script processes EIA 860 generator and plant data to calculate capacity value based on net load and LMP data.
@@ -20,6 +36,8 @@ This should be output for each facility as a time series of hourly prices.
 """
 # only need data for plants that are > 10 MW
 generators = load_eia_860(file="Generator", sheet="Operable", year=data_year)
+convert_to_type(generators, "Plant Code", "Plant Code", int)
+
 
 # sum "Nameplate Capacity (MW)" for each "Plant Code"
 plant_capacity = generators.groupby("Plant Code")["Nameplate Capacity (MW)"].sum()
@@ -44,6 +62,8 @@ generators = generators[generators["Plant Code"].isin(plant_capacity.index)]
 
 # Use plant file to get BA Code
 plants = load_eia_860(file="Plant", sheet="Plant", year=data_year)
+convert_to_type(plants, "Plant Code", "Plant Code", int)
+# convert_to_type(plants, "Primary Purpose (NAICS Code)", "Primary Purpose (NAICS Code)", int)
 
 # Primary Purpose (NAICS Code) only keep plants with code 22
 plants = plants[plants["Primary Purpose (NAICS Code)"] == 22]
@@ -66,9 +86,7 @@ net_cone["OTHER"] = np.average(list(net_cone.values()))
 net_cone_per_kw_year = {iso: value * 12 for iso, value in net_cone.items()}  # $/kw-year
 
 # load LMP plant id to price node mapper
-price_node_mapper_path = (
-    "/projects/surint/campd_spheres/nationwide/facility_lmp_mapping(in).csv"
-)
+
 price_node_cols = [
     "Plant Code",
     "Generator ID",
@@ -78,6 +96,7 @@ price_node_cols = [
     "Mapped_Price_Node_ID",
 ]
 price_node_mapper = pd.read_csv(price_node_mapper_path, usecols=price_node_cols)
+convert_to_type(price_node_mapper, "Plant Code", "Plant Code", int)
 price_node_mapper = price_node_mapper[
     price_node_mapper["Prime Mover"].isin(prime_movers)
 ]
@@ -93,8 +112,9 @@ price_node_mapper_iso_rename = {
     "PJM ISO": "PJM",
     "California ISO": "CISO",
     "ERCOT ISO": "ERCO",
-    "New England ISO": "???",  # what is this?
+    "New England ISO": "OTHER",  # what is this?
     "New York ISO": "NYIS",
+    "Northern Maine Independent System A": "OTHER",
 }
 
 # rename ISOs in price node mapper
@@ -102,18 +122,55 @@ price_node_mapper.replace(
     to_replace={"Mapped_ISO_Name": price_node_mapper_iso_rename}, inplace=True
 )
 
+price_node_mapper.set_index(keys=["Plant Code"], inplace=True)
+
 
 # for each unique plant code in generators
 # net_load = pd.read_csv(path + f"EIA930_BALANCE_{data_year}_with_Net_Load.csv")
 # NOTE: or should we be using Net_Load.csv?
-net_load1 = pd.read_csv(net_load_dir + f"EIA930_BALANCE_{data_year}_Jan_Jun.csv")
-net_load2 = pd.read_csv(net_load_dir + +f"EIA930_BALANCE_{data_year}_Jul_Dec.csv")
-net_load_df = pd.concat([net_load1, net_load2], axis=1)
+# net_load1 = pd.read_csv(net_load_dir + f"EIA930_BALANCE_{data_year}_Jan_Jun.csv")
+# net_load2 = pd.read_csv(net_load_dir + +f"EIA930_BALANCE_{data_year}_Jul_Dec.csv")
+# net_load_df = pd.concat([net_load1, net_load2], axis=1)
+net_load_df = pd.read_csv(net_load_dir / net_load_fname)
+time_profile = pd.to_datetime(net_load_df["UTC Time at End of Hour"], utc=True)
+net_load_df["Year (UTC)"] = [
+    time_profile.iloc[i].year for i in range(len(time_profile))
+]
+net_load_df["Time Profile (UTC)"] = time_profile
+# Remove data from years that aren't the data-year
+net_load_df = net_load_df[net_load_df["Year (UTC)"] == data_year]
+
+# fill nans with zero
+net_load_df["Net_Load"] = net_load_df["Net_Load"].fillna(0)
+
+# Get list of balancing authorities that don't have full timeseries data
+load_bas = net_load_df["Balancing Authority"].unique()
 net_load_df.set_index(keys="Balancing Authority", inplace=True)
+non_yearly_bas = {
+    k: len(net_load_df.loc[k]) for k in load_bas if len(net_load_df.loc[k]) != 8760
+}
 
-
+# NOTE: only Balancing authority that does not have yearly net load
+# info and is in the `plants`` dataframe is 'WWA'
+# and its only for 1 site (plant code 57995), which is a wind plant
+# plant_codes = [879]
 for p in plants["Plant Code"].unique():
-    inputs = {"plant_code": [p]}
+    # plant id 69875 has 1 generatior, so does 69748
+    # Get balancing authority code for this plant (for net-load data)
+    ba_code = plants.loc[plants["Plant Code"] == p, "Balancing Authority Code"].values[
+        0
+    ]
+
+    if isinstance(price_node_mapper.loc[p, "Mapped_ISO_Name"], str):
+        # just one generator in the price_node_mapper
+        iso_name = price_node_mapper.loc[p, "Mapped_ISO_Name"]
+        price_node_id = price_node_mapper.loc[p, "Mapped_Price_Node_ID"]
+    else:
+        # Get ISO name
+        iso_name = price_node_mapper.loc[p, "Mapped_ISO_Name"].values[0]
+        price_node_id = price_node_mapper.loc[p, "Mapped_Price_Node_ID"].values[0]
+
+    # inputs = {"plant_code": [p]}
 
     # --------- Load LMP file
     # pull energy market price per plant
@@ -122,33 +179,40 @@ for p in plants["Plant Code"].unique():
     # path = "/projects/surint/campd_spheres/nationwide/"
     # Get LMP Price node name from price_node_mapper
     # Get LMP Price node number from price node mapper
+    # lmp = pd.read_csv(lmp_dir + f"plant_{int(inputs['plant_code'][0])}.csv")
 
-    lmp = pd.read_csv(lmp_dir + f"plant_{int(inputs['plant_code'][0])}.csv")
+    lmp_fpath = lmp_dir / f"{price_node_id}_{data_year}.csv"
+    if lmp_fpath.exists():
+        lmp = pd.read_csv(lmp_fpath)
+        lmp["Time Profile (UTC)"] = pd.to_datetime(
+            lmp["GMT Datetime (Hour Ending)"], utc=True
+        )
+        lmp.set_index(keys=["Time Profile (UTC)"], inplace=True)
+        lmp.sort_index(inplace=True)
 
-    lmp_da = lmp[
-        "LMP_DA"
-    ].values  # TODO: local time? UTC? get correct year, units MW or kw??
+        lmp_da = lmp["Price ($/MWh)"].values
 
-    # -------- Load correct BA net load
-    ba_code = plants.loc[plants["Plant Code"] == p, "Balancing Authority Code"].values[
-        0
-    ]
+        # TODO: add check in that lmp_da is 8760
 
-    # net_load = net_load[net_load["Balancing Authority"] == ba_code]
+    # lmp_da = lmp["LMP_DA"].values
+    # TODO: local time? UTC? get correct year, units MW or kw??
+
+    # ----- Get Net Load For Balancing Authority-----
     net_load = net_load_df.loc[ba_code].copy(deep=True)
-
-    net_load.set_index("UTC Time at End of Hour", inplace=True)
-    net_load.index = pd.to_datetime(net_load.index, utc=True)
+    net_load.set_index("Time Profile (UTC)", inplace=True)
     net_load.sort_index(inplace=True)
 
-    # fill nans with zero
-    net_load = net_load.fillna(0)
-
     # get net load for capacity value calculation
-    net_load = net_load[
-        "Net_Load"
-    ].values  # TODO: local time? UTC? want net load values starting at 00:00 UTC
+    net_load = net_load["Net_Load"].values * 1000  # MW -> kW
+    # TODO: local time? UTC? want net load values starting at 00:00 UTC
     # Columns UTC Time at End of Hour, Local Time at End of Hour, Hour Number, Data Date
+
+    if len(net_load) != 8760:
+        msg = (
+            f"Facility ID {p} (in Balancing Authority {ba_code}) "
+            f"has Net_Load of length {len(net_load)}"
+        )
+        warnings.warn(msg, UserWarning, stacklevel=3)
 
     # get the top percentage of net load hours to use for capacity value calculation
     percentage_net_load = 0.03
@@ -163,7 +227,9 @@ for p in plants["Plant Code"].unique():
     total_peak_vals = np.sum(top_net_load)
     net_load_ratio = top_net_load / total_peak_vals
 
-    if ba_code not in net_cone:
-        ba_code = "OTHER"
+    if iso_name not in net_cone:  # NOTE: balancing authority or ISO?
+        iso_name = "OTHER"
     # distributed the net_cone_per_kw_year over highest net load hours of the year
-    capacity_value = net_cone_per_kw_year[ba_code] * net_load_ratio  # $/kw
+    capacity_value = net_cone_per_kw_year[iso_name] * net_load_ratio  # $/kw
+
+    # TODO: save capacity_value + lmp_da to a csv file named as the facility name
