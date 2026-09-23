0f28187b2abfbcfca1d29a1d3de31689e625d091
elenya-grant <116225007+elenya-grant@users.noreply.github.com>
2026-09-23 17:14:22 -0600

    fixes to plant config and wind tech and setup_tools

--- examples/run_exisiting_plant_with_add_on.py
+++ examples/run_exisiting_plant_with_add_on.py
@@ -1,32 +1,42 @@
-from nice.tools.file_tools import load_yaml, write_yaml, check_create_folder
-from nice import LIBRARY_DIR
 import shutil
-import pandas as pd
 from pathlib import Path
-from nice.simulation.setup_tools import get_techs_from_tech_connections, make_tech_config
-example_main_folder = LIBRARY_DIR / "h2i" # / "tech_config"
+
+import pandas as pd
+
+from nice import LIBRARY_DIR
+from nice.simulation.setup_tools import (
+    get_techs_from_tech_connections,
+    make_tech_config,
+)
+from nice.tools.file_tools import check_create_folder, load_yaml, write_yaml
+
+example_main_folder = LIBRARY_DIR / "h2i"  # / "tech_config"
 example_copy_dir = LIBRARY_DIR / "h2i_single_site"
 
 check_create_folder(example_copy_dir)
 
+
 def make_single_site_doe_csv(fac_id, original_doe_csv_fpath, copy_doe_csv_fpath):
     df = pd.read_csv(original_doe_csv_fpath)
-    fac_id_cols = [k for k in df.columns.to_list() if ".plant_code" in k or ".facility_id" in k]
+    fac_id_cols = [
+        k for k in df.columns.to_list() if ".plant_code" in k or ".facility_id" in k
+    ]
     fac_id_col = fac_id_cols[0]
     df[fac_id_col] = df[fac_id_col].astype(int)
-    sub_df = df[df[fac_id_col]==fac_id]
+    sub_df = df[df[fac_id_col] == fac_id]
     sub_df.to_csv(copy_doe_csv_fpath, index=False)
 
 
-
-def make_copy_of_example(case_subfolder_name, copy_case_subfolder_name, case_facility_id):
-    original = example_main_folder/case_subfolder_name
-    new_dir = example_copy_dir/copy_case_subfolder_name
+def make_copy_of_example(
+    case_subfolder_name, copy_case_subfolder_name, case_facility_id
+):
+    original = example_main_folder / case_subfolder_name
+    new_dir = example_copy_dir / copy_case_subfolder_name
 
     check_create_folder(new_dir)
 
     # make doe csv file
-    new_doe_fpath = new_dir/f"design_sweep_facility_{case_facility_id}.csv"
+    new_doe_fpath = new_dir / f"design_sweep_facility_{case_facility_id}.csv"
     if "one_axis" in copy_case_subfolder_name:
         driver_desc = "_one_axis"
         doe_filenames = [f for f in original.glob("*.csv") if "_one_axis_" in f.name]
@@ -38,39 +48,49 @@ def make_copy_of_example(case_subfolder_name, copy_case_subfolder_name, case_fac
     else:
         driver_desc = ""
         doe_fpath = list(original.glob("*.csv"))[0]
-    make_single_site_doe_csv(case_facility_id, doe_fpath, new_doe_fpath)
-    
+    if not doe_fpath.name.startswith("design_sweep_facility_"):
+        make_single_site_doe_csv(case_facility_id, doe_fpath, new_doe_fpath)
+
     # copy the driver config file, update the path to the new doe csv file
     driver_file = list(original.glob(f"driver_config{driver_desc}*"))[0]
     driver_config = load_yaml(driver_file)
     driver_config["driver"]["parameter_sweep"]["filename"] = str(new_doe_fpath)
-    driver_config["general"]["folder_output"] = str(new_dir/"outputs")
-    write_yaml(str(new_dir/"driver_config.yaml"), driver_config)
+    driver_config["general"]["folder_output"] = str(new_dir / "outputs")
+    write_yaml(str(new_dir / "driver_config.yaml"), driver_config)
     # shutil.copy(original/driver_file, new_dir/"driver_config.yaml")
-    
+
     # copy finance_groups file
-    shutil.copy(example_main_folder/"finance_groups.yaml", new_dir/"finance_groups.yaml")
-    
+    shutil.copy(
+        example_main_folder / "finance_groups.yaml", new_dir / "finance_groups.yaml"
+    )
+
     # copy plant config file, updating the path to the finance_groups.yaml file
-    with Path(original/"plant_config.yaml").open(mode="r", encoding="utf-8") as file:
+    with Path(original / "plant_config.yaml").open(mode="r", encoding="utf-8") as file:
         lines = file.readlines()
         txt = "".join(l for l in lines)
 
     old_line = "finance_groups: !include ./finance_groups.yaml"
-    finance_group_fpath = str(new_dir/"finance_groups.yaml")
+    finance_group_fpath = str(new_dir / "finance_groups.yaml")
     new_line = f"finance_groups: !include {finance_group_fpath}"
     new_txt = txt.replace(old_line, new_line)
+    new_txt = new_txt.replace(
+        "resource_to_tech_connections", "site_to_tech_connections"
+    )
 
-    with Path(new_dir/"plant_config.yaml").open(mode="w", encoding="utf-8") as file:
+    with Path(new_dir / "plant_config.yaml").open(mode="w", encoding="utf-8") as file:
         file.write(new_txt)
 
-    shutil.copytree(original, new_dir, dirs_exist_ok=True)
+    # shutil.copytree(original, new_dir, dirs_exist_ok=True)
 
 
 if __name__ == "__main__":
     from h2integrate import H2IntegrateModel
     from h2integrate.core.file_utils import check_file_format_for_csv_generator
-    
+
+    run_parallel = False
+    debug_print = True
+    run_h2i = True
+    save_debug_config_files = False
     # example_facilities = [1355, 7526, 7790, 9]
     # 7790_2025_price_profile.csv
     # thermal facility: 9
@@ -90,60 +110,92 @@ if __name__ == "__main__":
         "thermal": "thermal",
         "one_axis_solar": "solar",
         "fixed_solar": "solar",
-        "wind": "wind"
+        "wind": "wind",
     }
 
-    add_on_cases = ["solar","battery","solar_battery"]
+    add_on_cases = ["solar", "battery", "solar_battery"]
+    # add_on_cases = ["solar"]
 
     for existing_case_desc, existing_plant_type in existing_cases.items():
         case_fac_id = existing_cases_to_facilities[existing_case_desc]
 
         if existing_plant_type == "solar":
-            existing_pv_type = existing_case_desc.replace("_solar","")
+            existing_pv_type = existing_case_desc.replace("_solar", "")
         else:
             existing_pv_type = "one_axis"
         for add_on_case in add_on_cases:
+            print(f"Starting existing {existing_case_desc} add-on {add_on_case}")
 
             original_subdir_name = f"existing_{existing_plant_type}_add_{add_on_case}"
             new_subdir_name = f"existing_{existing_case_desc}_add_{add_on_case}"
 
             make_copy_of_example(original_subdir_name, new_subdir_name, case_fac_id)
 
-            case_folder = example_copy_dir/new_subdir_name
+            case_folder = example_copy_dir / new_subdir_name
 
             plant_config = load_yaml(case_folder / "plant_config.yaml")
             tech_names = get_techs_from_tech_connections(
-                    plant_config["technology_interconnections"]
-                )
-            tech_config = make_tech_config(tech_names, existing_plant_type, add_on_case, existing_pv_type=existing_pv_type)
+                plant_config["technology_interconnections"]
+            )
+            tech_config = make_tech_config(
+                tech_names,
+                existing_plant_type,
+                add_on_case,
+                existing_pv_type=existing_pv_type,
+            )
 
             # save tech config file, for debugging purposes
-            tech_config_fpath = case_folder/"tech_config.yaml"
+            tech_config_fpath = case_folder / "tech_config.yaml"
             write_yaml(str(tech_config_fpath), tech_config)
 
-            driver_config = load_yaml(case_folder/"driver_config.yaml")
+            driver_config = load_yaml(case_folder / "driver_config.yaml")
+
+            driver_config["driver"]["parameter_sweep"]["run_parallel"] = run_parallel
+            driver_config["driver"]["parameter_sweep"]["debug_print"] = debug_print
+
+            for tech_name, tech_vars in driver_config["design_variables"].items():
+                for tech_var, tech_var_stuff in tech_vars.items():
+                    if (
+                        "lower" in tech_var_stuff
+                        and tech_var_stuff.get("lower") is None
+                    ):
+                        driver_config["design_variables"][tech_name][tech_var][
+                            "lower"
+                        ] = -1e6
+                    if (
+                        "upper" in tech_var_stuff
+                        and tech_var_stuff.get("upper") is None
+                    ):
+                        driver_config["design_variables"][tech_name][tech_var][
+                            "upper"
+                        ] = 1e6
+
+            if save_debug_config_files:
+                write_yaml(str(case_folder / "check_driver_cnfg.yaml"), driver_config)
+                write_yaml(str(case_folder / "check_plnt_cnfg.yaml"), plant_config)
 
             config = {
                 "plant_config": plant_config,
-                "tech_config": tech_config,
+                "technology_config": tech_config,
                 "driver_config": driver_config,
             }
 
             # Update formatting for CSV file
-            
+
             check_file_format_for_csv_generator(
                 Path(driver_config["driver"]["parameter_sweep"]["filename"]),
                 driver_config,
                 check_only=False,
                 overwrite_file=True,
             )
-            
+
+            print(f"Done setting-up {existing_case_desc} add-on {add_on_case}")
 
             # TODO: add H2I run in!
-            print(f"Starting existing {existing_case_desc} add-on {add_on_case}")
+            # print(f"Starting existing {existing_case_desc} add-on {add_on_case}")
 
-            h2i = H2IntegrateModel(config)
-            h2i.setup()
-            h2i.run()
-            
-
+            if run_h2i:
+                print(f"Running H2I for {existing_case_desc} add-on {add_on_case}")
+                h2i = H2IntegrateModel(config)
+                h2i.setup()
+                h2i.run()
--- nice/simulation/setup_tools.py
+++ nice/simulation/setup_tools.py
@@ -1,11 +1,13 @@
 # from h2integrate import H2Integrate
 from nice import LIBRARY_DIR, ROOT_DIR
+
 # from h2integrate.core.file_utils import load_yaml
 # from h2integrate.core.inputs.validation import load_tech_yaml, load_plant_yaml, load_driver_yaml
-from nice.tools.file_tools import load_yaml, check_create_folder
+from nice.tools.file_tools import check_create_folder, load_yaml
 
-tech_config_folder = LIBRARY_DIR/"h2i"/"tech_config"
-sql_output_dir = ROOT_DIR.parent/"doe_results"
+tech_config_folder = LIBRARY_DIR / "h2i" / "tech_config"
+sql_output_dir = ROOT_DIR.parent / "doe_results"
+
 
 def get_techs_from_tech_connections(tech_connections):
     techs = set()
@@ -14,20 +16,22 @@ def get_techs_from_tech_connections(tech_connections):
 
     return list(techs)
 
+
 def add_tech_to_config(tech_config, tech_name, tech_filename):
-    tech_params = load_yaml(tech_config_folder/tech_filename)
+    tech_params = load_yaml(tech_config_folder / tech_filename)
     if "model_inputs" in tech_params:
         tech_config[tech_name] = tech_params
     else:
         # file has tech names
         tech_config |= tech_params
-    
+
     return tech_config
 
+
 def make_tech_config(tech_names, existing_case, add_on_case, existing_pv_type="fixed"):
-    if existing_case =="solar" and existing_pv_type is None:
+    if existing_case == "solar" and existing_pv_type is None:
         raise ValueError("Existing pv type is required when existing_case is solar")
-    
+
     tech_config = {}
     always_expected_techs = ["existing_plant", "poi_demand", "grid_sell"]
     if not set(tech_names) >= set(always_expected_techs):
@@ -37,12 +41,18 @@ def make_tech_config(tech_names, existing_case, add_on_case, existing_pv_type="f
 
     tech_config = add_tech_to_config(tech_config, "grid_sell", "grid_sell.yaml")
     tech_config = add_tech_to_config(tech_config, "poi_demand", "poi_demand.yaml")
-    existing_tech_fname = f"existing_{existing_pv_type}_solar.yaml" if existing_case == "solar" else f"existing_{existing_case}.yaml"
+    existing_tech_fname = (
+        f"existing_{existing_pv_type}_solar.yaml"
+        if existing_case == "solar"
+        else f"existing_{existing_case}.yaml"
+    )
     tech_config = add_tech_to_config(tech_config, "existing_plant", existing_tech_fname)
     if add_on_case == "solar":
         tech_config = add_tech_to_config(tech_config, "solar_add_on", "add_solar.yaml")
     else:
-        tech_config = add_tech_to_config(tech_config, add_on_case, f"add_{add_on_case}.yaml")
+        tech_config = add_tech_to_config(
+            tech_config, add_on_case, f"add_{add_on_case}.yaml"
+        )
 
     demand_techs = [k for k in tech_names if "demand" in k]
     combiner_techs = [k for k in tech_names if "combiner" in k]
@@ -50,29 +60,31 @@ def make_tech_config(tech_names, existing_case, add_on_case, existing_pv_type="f
         tech_config = add_tech_to_config(tech_config, dmd_tech, "poi_demand.yaml")
     for combiner_tech in combiner_techs:
         tech_config = add_tech_to_config(tech_config, combiner_tech, "combiner.yaml")
-    
+
+    remaining_techs = set(tech_names) - set(tech_config)
+    for remaining_tech in remaining_techs:
+        tech_config = add_tech_to_config(
+            tech_config, remaining_tech, f"{remaining_tech}.yaml"
+        )
+
     techs_included = set(tech_config)
 
     extra_techs = techs_included - set(tech_names)
     not_found_techs = set(tech_names) - techs_included
     if bool(extra_techs):
-        msg = (
-            f"Extra techs added to tech_config: {sorted(extra_techs)}"
-        )
+        msg = f"Extra techs added to tech_config: {sorted(extra_techs)}"
         raise ValueError(msg)
     if bool(not_found_techs):
-        msg = (
-            f"Missing techs: {sorted(extra_techs)} in tech_config"
-        )
+        msg = f"Missing techs: {sorted(not_found_techs)} in tech_config"
         raise ValueError(msg)
-    
-    return {"technologies":tech_config}
-   
+
+    return {"technologies": tech_config}
+
 
 def check_format_csv_file(driver_config, doe_csv_path):
-    from h2integrate.core.dict_utils import update_defaults
     from h2integrate.core.file_utils import check_file_format_for_csv_generator
-    new_csv_filename = check_file_format_for_csv_generator(
+
+    check_file_format_for_csv_generator(
         doe_csv_path,
         driver_config,
         check_only=False,
@@ -88,11 +100,14 @@ def check_format_csv_file(driver_config, doe_csv_path):
 
     return driver_config
 
+
 def make_h2i_input_files(existing_plant_case, add_on_case, **existing_plant_kwargs):
     existing_plant_case = "wind"  # ["wind","solar","thermal"]
     add_on_case = "solar"  # ["solar","battery","solar_battery"]
 
-    case_folder = LIBRARY_DIR / "h2i" / f"existing_{existing_plant_case}_add_{add_on_case}"
+    case_folder = (
+        LIBRARY_DIR / "h2i" / f"existing_{existing_plant_case}_add_{add_on_case}"
+    )
     # plant_config = load_plant_yaml(case_folder/"plant_config.yaml")
     plant_config = load_yaml(case_folder / "plant_config.yaml")
     tech_names = get_techs_from_tech_connections(
@@ -100,24 +115,25 @@ def make_h2i_input_files(existing_plant_case, add_on_case, **existing_plant_kwar
     )
 
     # Make tech config file
-    if existing_plant_case=="solar" and "array_type" not in existing_plant_kwargs:
-        msg = (
-            "Please provide array_type in kwargs if existing plant is solar"
-        )
+    if existing_plant_case == "solar" and "array_type" not in existing_plant_kwargs:
+        msg = "Please provide array_type in kwargs if existing plant is solar"
         raise ValueError(msg)
-    elif existing_plant_case=="solar" and "array_type" in existing_plant_kwargs:
-        tech_config = make_tech_config(tech_names, existing_plant_case, add_on_case, existing_pv_type=existing_plant_kwargs["array_type"])
+    elif existing_plant_case == "solar" and "array_type" in existing_plant_kwargs:
+        tech_config = make_tech_config(
+            tech_names,
+            existing_plant_case,
+            add_on_case,
+            existing_pv_type=existing_plant_kwargs["array_type"],
+        )
 
     else:
         # NOTE: make_tech_config is not set-up to update the filename for the thermal plant tech yet
         tech_config = make_tech_config(tech_names, existing_plant_case, add_on_case)
 
-    if existing_plant_case=="thermal" and "n_facilities" not in existing_plant_kwargs:
-        msg = (
-            "Please provide n_facilities in kwargs if existing plant is thermal"
-        )
+    if existing_plant_case == "thermal" and "n_facilities" not in existing_plant_kwargs:
+        msg = "Please provide n_facilities in kwargs if existing plant is thermal"
         raise ValueError(msg)
-    
+
     # Make driver config file
     driver_config_filename = "driver_config.yaml"
 
@@ -127,28 +143,35 @@ def make_h2i_input_files(existing_plant_case, add_on_case, **existing_plant_kwar
         recorder_filename = f"existing_{existing_plant_case}_add_{add_on_case}_{n_facilities}_facilities.sql"
     elif existing_plant_case == "solar":
         pv_type = existing_plant_kwargs["array_type"]
-        doe_csv_filename = f"existing_{pv_type}_{existing_plant_case}_add_{add_on_case}_doe.csv"
+        doe_csv_filename = (
+            f"existing_{pv_type}_{existing_plant_case}_add_{add_on_case}_doe.csv"
+        )
         driver_config_filename = f"driver_config_{pv_type}.yaml"
-        recorder_filename = f"existing_{pv_type}_{existing_plant_case}_add_{add_on_case}.sql"
+        recorder_filename = (
+            f"existing_{pv_type}_{existing_plant_case}_add_{add_on_case}.sql"
+        )
     else:
         doe_csv_filename = f"existing_{existing_plant_case}_add_{add_on_case}_doe.csv"
         recorder_filename = f"existing_{existing_plant_case}_add_{add_on_case}.sql"
 
-    driver_config = load_yaml(case_folder/driver_config_filename)
+    driver_config = load_yaml(case_folder / driver_config_filename)
     driver_config["general"]["folder_output"] = sql_output_dir
-    driver_config["driver"]["parameter_sweep"]["filename"] = case_folder/doe_csv_filename
+    driver_config["driver"]["parameter_sweep"]["filename"] = (
+        case_folder / doe_csv_filename
+    )
     driver_config["recorder"]["file"] = recorder_filename
     check_create_folder(sql_output_dir)
-    check_format_csv_file(driver_config, case_folder/doe_csv_filename)
+    check_format_csv_file(driver_config, case_folder / doe_csv_filename)
 
     top_level_config = {
-        "driver_config":driver_config,
+        "driver_config": driver_config,
         "technology_config": tech_config,
         "plant_config": plant_config,
     }
     return top_level_config
 
-# if __name__ == "__main__":    
+
+# if __name__ == "__main__":
 #     existing_cases = ["thermal","solar","wind"]
 #     add_on_cases = ["solar","battery","solar_battery"]
 #     kwargs = {
